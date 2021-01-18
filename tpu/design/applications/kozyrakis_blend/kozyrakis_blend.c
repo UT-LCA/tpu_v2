b3121183e723485960be3e7506b7473204ec8c55
aman26kbm <aman.kbm@gmail.com>
2021-01-17 21:24:26 -0600

    Changes to Makefile to fix the path of the compiler. Also adding target to generate .read file by default. Also commenting out the code that is causing X poisoning for now (the read of the "counter" peripheral).

--- tpu/design/applications/kozyrakis_blend/kozyrakis_blend.c
+++ tpu/design/applications/kozyrakis_blend/kozyrakis_blend.c
@@ -14,15 +14,15 @@ volatile char out[WIDTH*HEIGHT];
 
 int main()
 {
-  start_count=*counter;   //Read counter to start benchmarking
+  //start_count=*counter;   //Read counter to start benchmarking
 
   int i;
   for (i=0; i<ITERATIONS; i++)
     out[i]=vector_blend(WIDTH*HEIGHT,frame1,frame2,0x80,out);
 
-  end_count=*counter;   //Read counter to end benchmarking
+  //end_count=*counter;   //Read counter to end benchmarking
 
-  printf("-- Target Duration = %d\n",end_count-start_count);
-  fflush(stdout);
+  //printf("-- Target Duration = %d\n",end_count-start_count);
+  //fflush(stdout);
   return 0;
 }
