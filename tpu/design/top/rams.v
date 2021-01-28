e0be9d396976662abc4c3755bf6a6016399bd9bf
aman26kbm <aman.kbm@gmail.com>
2021-01-27 21:32:10 -0600

    Updating to cleaner parameterized RAM models

--- tpu/design/top/rams.v
+++ tpu/design/top/rams.v
@@ -1,68 +1,62 @@
 module dpram (
-        addr0, 
-        d0, 
-        we0, 
-        q0,  
-        addr1,
-        d1,
-        we1,
-        q1,
-        clk);
+	clk,
+	address_a,
+	address_b,
+	wren_a,
+	wren_b,
+	data_a,
+	data_b,
+	out_a,
+	out_b
+);
 
-input [`AWIDTH-1:0] addr0;
-input [`AWIDTH-1:0] addr1;
-input [`MAT_MUL_SIZE*`DWIDTH-1:0] d0;
-input [`MAT_MUL_SIZE*`DWIDTH-1:0] d1;
-input [`MAT_MUL_SIZE-1:0] we0;
-input [`MAT_MUL_SIZE-1:0] we1;
-output [`MAT_MUL_SIZE*`DWIDTH-1:0] q0;
-output [`MAT_MUL_SIZE*`DWIDTH-1:0] q1;
+parameter AWIDTH=10;
+parameter NUM_WORDS=1024;
+parameter DWIDTH=32;
+
 input clk;
+input [(AWIDTH-1):0] address_a;
+input [(AWIDTH-1):0] address_b;
+input  wren_a;
+input  wren_b;
+input [(DWIDTH-1):0] data_a;
+input [(DWIDTH-1):0] data_b;
+output reg [(DWIDTH-1):0] out_a;
+output reg [(DWIDTH-1):0] out_b;
 
-`ifdef VCS
-reg [`MAT_MUL_SIZE*`DWIDTH-1:0] q0;
-reg [`MAT_MUL_SIZE*`DWIDTH-1:0] q1;
-reg [7:0] ram[((1<<`AWIDTH)-1):0];
-integer i;
+`ifdef SIMULATION_MEMORY
 
-always @(posedge clk)  
-begin 
-    for (i = 0; i < `MAT_MUL_SIZE; i=i+1) begin
-        if (we0[i]) ram[addr0+i] <= d0[i*`DWIDTH +: `DWIDTH]; 
-    end    
-    for (i = 0; i < `MAT_MUL_SIZE; i=i+1) begin
-        q0[i*`DWIDTH +: `DWIDTH] <= ram[addr0+i];
-    end    
+reg [DWIDTH-1:0] ram[NUM_WORDS-1:0];
+
+always @ (posedge clk) begin 
+  if (wren_a) begin
+      ram[address_a] <= data_a;
+  end
+  else begin
+      out_a <= ram[address_a];
+  end
 end
-
-always @(posedge clk)  
-begin 
-    for (i = 0; i < `MAT_MUL_SIZE; i=i+1) begin
-        if (we1[i]) ram[addr0+i] <= d1[i*`DWIDTH +: `DWIDTH]; 
-    end    
-    for (i = 0; i < `MAT_MUL_SIZE; i=i+1) begin
-        q1[i*`DWIDTH +: `DWIDTH] <= ram[addr1+i];
-    end    
+  
+always @ (posedge clk) begin 
+  if (wren_b) begin
+      ram[address_b] <= data_b;
+  end 
+  else begin
+      out_b <= ram[address_b];
+  end
 end
 
 `else
-//BRAMs available in VTR FPGA architectures have one bit write-enables.
-//So let's combine multiple bits into 1. We don't have a usecase of
-//writing/not-writing only parts of the word anyway.
-wire we0_coalesced;
-assign we0_coalesced = |we0;
-wire we1_coalesced;
-assign we1_coalesced = |we1;
 
 dual_port_ram u_dual_port_ram(
-.addr1(addr0),
-.we1(we0_coalesced),
-.data1(d0),
-.out1(q0),
-.addr2(addr1),
-.we2(we1_coalesced),
-.data2(d1),
-.out2(q1),
+.addr1(address_a),
+.we1(wren_a),
+.data1(data_a),
+.out1(out_a),
+.addr2(address_b),
+.we2(wren_b),
+.data2(data_b),
+.out2(out_b),
 .clk(clk)
 );
 
@@ -71,45 +65,43 @@ dual_port_ram u_dual_port_ram(
 endmodule
 
 module spram (
-        addr0, 
-        d0, 
-        we0, 
-        q0,  
-        clk);
+    clk,
+	address,
+	wren,
+	data,
+	out
+);
 
-input [`AWIDTH-1:0] addr0;
-input [`MAT_MUL_SIZE*`DWIDTH-1:0] d0;
-input [`MAT_MUL_SIZE-1:0] we0;
-output [`MAT_MUL_SIZE*`DWIDTH-1:0] q0;
+parameter AWIDTH=10;
+parameter NUM_WORDS=1024;
+parameter DWIDTH=32;
+
 input clk;
+input [(AWIDTH-1):0] address;
+input  wren;
+input [(DWIDTH-1):0] data;
+output reg [(DWIDTH-1):0] out;
 
-`ifdef VCS
-reg [`MAT_MUL_SIZE*`DWIDTH-1:0] q0;
-reg [7:0] ram[((1<<`AWIDTH)-1):0];
-integer i;
+`ifdef SIMULATION_MEMORY
 
-always @(posedge clk)  
-begin 
-    for (i = 0; i < `MAT_MUL_SIZE; i=i+1) begin
-        if (we0[i]) ram[addr0+i] <= d0[i*`DWIDTH +: `DWIDTH]; 
-    end    
-    for (i = 0; i < `MAT_MUL_SIZE; i=i+1) begin
-        q0[i*`DWIDTH +: `DWIDTH] <= ram[addr0+i];
-    end    
+reg [DWIDTH-1:0] ram[NUM_WORDS-1:0];
+
+always @ (posedge clk) begin 
+  if (wren) begin
+      ram[address] <= data;
+  end
+  else begin
+      out <= ram[address];
+  end
 end
-
+  
 `else
-//BRAMs available in VTR FPGA architectures have one bit write-enables.
-//So let's combine multiple bits into 1. We don't have a usecase of
-//writing/not-writing only parts of the word anyway.
-wire we0_coalesced;
-assign we0_coalesced = |we0;
 
 single_port_ram u_single_port_ram(
-.addr(addr0),
-.we(we0_coalesced),
-.data(d0),
-.out(q0),
+.addr(address),
+.we(wren),
+.data(data),
+.out(out),
 .clk(clk)
 );
 
