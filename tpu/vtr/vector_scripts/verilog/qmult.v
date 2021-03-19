effc7c21540ca824f5cd94caae5a80d3e19d3861
Aatman Borda <borda.aatman@gmail.com>
2021-03-19 21:11:08 +0530

    removed script generated files

--- tpu/vtr/vector_scripts/verilog/qmult.v
+++
@@ -1,9 +0,0 @@
-module qmult(i_multiplicand,i_multiplier,o_result);
-input [`DWIDTH-1:0] i_multiplicand;
-input [`DWIDTH-1:0] i_multiplier;
-output [2*`DWIDTH-1:0] o_result;
-
-assign o_result = i_multiplicand * i_multiplier;
-//DW02_mult #(`DWIDTH,`DWIDTH) u_mult(.A(i_multiplicand), .B(i_multiplier), .TC(1'b1), .PRODUCT(o_result));
-
-endmodule
